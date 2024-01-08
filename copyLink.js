891a079403b37906c8871004ecdcf889619fd80d
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2024-01-08 09:46:45 +0900

    Show original copy button and apply rainbow background to custom button

--- copyLink.js
+++ copyLink.js
@@ -5,7 +5,6 @@ function copyLink(observer) {
   
   const hyperlinkCopyLinkElement = originalCopyLinkElement.cloneNode(true)
 
-  originalCopyLinkElement.style.display = 'none';
   hyperlinkCopyLinkElement.removeEventListener('click', null);
   hyperlinkCopyLinkElement.removeAttribute('class');
   hyperlinkCopyLinkElement.classList.add('TaskPaneToolbar-copyLinkButton--hyperlink');
@@ -50,7 +49,20 @@ function copyLink(observer) {
     }
     
     .TaskPaneToolbar-copyLinkButton--hyperlink:hover {
-      background-color: #2E3031;
+      background: linear-gradient(
+        90deg,
+        rgba(255, 0, 0, 1) 0%,
+        rgba(255, 154, 0, 1) 10%,
+        rgba(208, 222, 33, 1) 20%,
+        rgba(79, 220, 74, 1) 30%,
+        rgba(63, 218, 216, 1) 40%,
+        rgba(47, 201, 226, 1) 50%,
+        rgba(28, 127, 238, 1) 60%,
+        rgba(95, 21, 242, 1) 70%,
+        rgba(186, 12, 248, 1) 80%,
+        rgba(251, 7, 217, 1) 90%,
+        rgba(255, 0, 0, 1) 100%
+      );
     }
     
     .TaskPaneToolbar-copyLinkButton--hyperlink:hover > svg {
